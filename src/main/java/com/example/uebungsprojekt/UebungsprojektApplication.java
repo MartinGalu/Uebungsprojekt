ccca98517f3064e88d107a0a6d32d62ce4558720
Martin Galusinski <martin.galusinski@dhl.com>
2022-09-26 11:54:07 +0200

    Initial commit

---
+++ src/main/java/com/example/uebungsprojekt/UebungsprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.uebungsprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungsprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UebungsprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uebungsprojekt/UebungsprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uebungsprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungsprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
