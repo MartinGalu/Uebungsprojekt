e99fe59f7079f9bdcc47a215c74ff18ebb4debd2
Xun <16625871+MartinGalu@users.noreply.github.com>
2022-09-27 12:04:37 +0200

    run tests with testcontainer

--- src/test/java/com/example/uebungsprojekt/UebungsprojektApplicationTests.java
+++ src/test/java/com/example/uebungsprojekt/UebungsprojektApplicationTests.java
@@ -3,7 +3,10 @@ package com.example.uebungsprojekt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(properties = {
+        "spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
+        "spring.datasource.url=jdbc:tc:postgresql:///postgres"})
 class UebungsprojektApplicationTests {
 
     @Test
